2081db548bc8d0c4165a556b37e7e20165ff7b8e
buenni86 <arslan.buenyamin@gmail.com>
2024-05-28 17:00:48 +0200

    Update

--- map_script.js
+++ map_script.js
@@ -7,15 +7,6 @@ import { bootstrapExtra } from "@workadventure/scripting-api-extra";
 console.log("Script started successfully")
 
 var currentPopup = undefined;
-var isCoWebSiteOpened =  false;
-var urlTutorial = "https://web.microsoftstream.com/embed/video/ca24bcea-3cab-4878-8b34-65e6bf87939f?autoplay=true";
-var zoneTutorial = "tutorial";
-
-var popUpStart = "popUpStart";
-var startMsg = "Willkommen bei unserer Live-Demo von DB WorkAdventure zum Digital Workplace Anwendertag!\n\nErkunde unsere Umgebung mit deinem Avatar, um die verschiedenen Funktionen und Möglichkeiten kennenzulernen. Nutze die Chance und #VernetzeDich mit anderen Teilnehmern!\nIm Programm des Anwendertages kannst du unseren Vortrag zu digital Eventplattformen bzw. unseren Markstand zur ortsunabhängigen Zusammenarbeit in virtuellen Büros besuchen, um mehr zu erfahren!";
-var popUpEmail = "popUpEmail";
-var mailMsg = "Bingo? Sende uns deine Bilder um zu gewinnen!";
-var mailToEvs = "mailto:SendIn.Enterprise.VoIP.Services@deutschebahn.com?subject=WA-Mail";
 
 		
 function closePopUp(){
@@ -24,20 +15,6 @@ function closePopUp(){
         currentPopup = undefined;
     }
 }
- 
-  WA.room.onEnterLayer("start_zone").subscribe(() => {
-    currentPopup =   WA.ui.openPopup(popUpStart, startMsg,[
-        {
-            label: "OK",
-            callback: (popup => {
-                closePopUp();
-            })
-        }]);
-});
-
-WA.room.onLeaveLayer("start_zone").subscribe(() => {
-    closePopUp();
-})
 
 WA.room.onEnterLayer("ticket_program").subscribe(() => {
    currentPopup =  WA.ui.openPopup("popUpTicket","Mitarbeiterfahrkarten bestellen?",[
@@ -75,18 +52,6 @@ WA.onInit().then(async () => {
 	
     console.log("Scripting API ready")
     console.log("Player tags: ", WA.player.tags)
-    var pos= await WA.player.getPosition()
-   if(pos.x<2100){
-        currentPopup =   WA.ui.openPopup(popUpStart, startMsg,[
-    
-        {
-            label: "OK",
-            callback: (popup => {
-                closePopUp();
-            })
-        }]);
-
-   }
     
       // The line below bootstraps the Scripting API Extra library that adds a number of advanced properties/features to WorkAdventure
       bootstrapExtra()
